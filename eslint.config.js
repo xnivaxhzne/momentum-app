233068603cdc89e0872e8373d8e356e1f16886f8
xnivaxhzne <kavinkumar1543884@gmail.com>
2025-07-23 00:02:28 +0530

    Add prettier plugin for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -19,5 +20,11 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser
     }
-  }
+  },
+  {
+    rules: {
+      'react-refresh/only-export-components': 'off'
+    }
+  },
+  eslintPluginPrettierRecommended
 ])
